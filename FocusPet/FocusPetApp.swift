fb9ee52a3a678cfd994ab9965eaa77618ed13f79
xiaoshy <xiaoshy25@mail2.sysu.edu.cn>
2026-03-14 17:16:48 +0800

    Initial Commit

---
+++ FocusPet/FocusPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusPetApp.swift
+//  FocusPet
+//
+//  Created by xsy on 2026/3/14.
+//
+
+import SwiftUI
+
+@main
+struct FocusPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
